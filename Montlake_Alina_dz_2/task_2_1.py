3b8ee9c82e0455a80d1e51b228a75ce3fe711af9
alinamontlake <evsyukovaalinaa@gmail.com>
2022-03-29 21:30:15 +0300

    Выполнила ДЗ№2

---
+++ Montlake_Alina_dz_2/task_2_1.py
@@ -0,0 +1,18 @@
+"1.Выяснить тип результата выражений:"
+
+"a"
+first_result = 15 * 3
+print (type(first_result))
+
+"b"
+second_result = 15 / 3
+print (type(second_result))
+
+"c"
+third_result = 15 // 3
+print (type(third_result))
+
+"d"
+fourth_result = 15 ** 3
+print (type(fourth_result))
+
